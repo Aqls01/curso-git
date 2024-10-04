a56533cb561002379dca8973429308b942bacf98
Aqls01 <1600055@senati.pe>
2024-10-04 11:13:36 -0500

    create index.html

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+}
+function showAlert(){
+Swal.fire({
+    title: "Greniu.",
+    width: 600,
+    padding: "3em",
+    color: "#716add",
+    background: "#fff",
+    backdrop: `
+      rgba(0,0,123,0.4)
+      url("ma.gif")
+      left top
+      no-repeat
+    `
+  });
+}
